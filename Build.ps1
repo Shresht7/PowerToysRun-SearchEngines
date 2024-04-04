ed8f896905a3e3a62b301d4b29e5b1cf67082243
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-04-04 22:00:32 +0530

    Create build script

---
+++ Build.ps1
@@ -0,0 +1,27 @@
+$ProjectName = "SearchEngines" # Project Name
+$ProjectFullname = "Community.PowerToys.Run.Plugin.$ProjectName" # Project FullName
+$Version = "1.0.0" # Plugin Version Number
+
+# Supported Platforms
+$Platforms = @(
+    "x64"
+    # , "ARM64"
+)
+
+# Build the project for each platform
+foreach ($Platform in $Platforms) {
+
+    # Clean the bin folder
+    if (Test-Path -Path "$PSScriptRoot\$ProjectFullName\bin") {
+        Remove-Item -Path "$PSScriptRoot\$ProjectFullName\bin\*" -Recurse -Force
+    }
+
+    # Build the project
+    dotnet build "$PSScriptRoot\$ProjectFullName.sln" -c Release /p:Platform=$Platform
+
+    # Package the project
+    Remove-Item -Path "$PSScriptRoot\$ProjectFullName\bin\*" -Recurse -Include *.xml, *.pdb, PowerToys.*, Wox.*
+    Rename-Item -Path "$PSScriptRoot\$ProjectFullName\bin\$Platform\Release" -NewName $ProjectName    
+    Compress-Archive -Path "$PSScriptRoot\$ProjectFullName\bin\$Platform\$ProjectName" -DestinationPath "$PSScriptRoot\Dist\$ProjectName-$Version-$Platform.zip"
+
+}
